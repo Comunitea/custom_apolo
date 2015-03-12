7a855e1b9fe21cd6826af169e3a53069d37e5ac7
Omar (Pexego) <omarcs7r@gmail.com>
2015-03-12 19:39:27 +0100

    [FIX] importation: typo

--- importation/import_customers.py
+++ importation/import_customers.py
@@ -14,7 +14,7 @@ class import_customers(object):
         try:
             self.url_template = "http://%s:%s/xmlrpc/%s"
             self.server = "localhost"
-            self.port = 9069
+            self.port = 8069
             self.dbname = dbname
             self.user_name = user
             self.user_passwd = passwd
