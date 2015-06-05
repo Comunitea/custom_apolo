2a20452dff27400efa2c2b9c602aa744cf7b503f
Omar <omarcs7r@gmail.com>
2015-06-05 13:45:52 +0200

    [ADD] importation: Añadimos un script d eiomportación de precios y una mejora para actualizar los productos

--- importation/import_pricelist.py
+++ importation/import_pricelist.py
@@ -7,19 +7,8 @@ import socket
 import traceback
 import xlrd
 
-UOM_MAP = {'C': "Box(es)",
-           'U': "Unit(s)",
-           'K': "kg",
-           'L': "Liter(s)"}
-IVA_MAP = {
-    "1": ["S_IVA10", "P_IVA10_BC"],
-    "2": ["S_IVA4", "P_IVA4_BC"],
-    "3": ["S_IVA21", "P_IVA21_BC"],
-    "4": ["S_IVA0", "P_IVA0_BC"]
-}
-
-class import_products(object):
-    def __init__(self, dbname, user, passwd, products_file):
+class import_prices(object):
+    def __init__(self, dbname, user, passwd, prices_file):
         """método incial"""
 
         try:
@@ -29,7 +18,7 @@ class import_products(object):
             self.dbname = dbname
             self.user_name = user
             self.user_passwd = passwd
-            self.products_file = products_file
+            self.prices_file = prices_file
 
             #
             # Conectamos con OpenERP
@@ -38,7 +27,7 @@ class import_products(object):
             self.user_id = login_facade.login(self.dbname, self.user_name, self.user_passwd)
             self.object_facade = xmlrpclib.ServerProxy(self.url_template % (self.server, self.port, 'object'))
 
-            res = self.import_product()
+            res = self.import_pricelist()
             #con exito
             if res:
                 print ("All created")
@@ -177,91 +166,21 @@ class import_products(object):
             categ_ids = [categ_id]
         return categ_ids and categ_ids[-1] or False
 
-    def import_product(self):
-        pwb = xlrd.open_workbook(self.products_file, encoding_override="utf-8")
+    def import_pricelist(self):
+        pwb = xlrd.open_workbook(self.prices_file, encoding_override="utf-8")
         sh = pwb.sheet_by_index(0)
 
         cont = 1
         all_lines = sh.nrows - 1
-        print "products no: ", all_lines
+        print "prices no: ", all_lines
         for rownum in range(1, all_lines):
             record = sh.row_values(rownum)
             try:
-                product_vals = {
-                    "default_code": str(int(record[0])),
-                    "name": record[4],
-                    "categ_id": self.getCategoryByName(record[6]),
-                    "uom_id": 1, # Unidad
-                    "uom_po_id": record[7] in ["C","U"] and self.getUomByName(UOM_MAP[record[7]]) or 1,
-                    "uos_id": self.getUomByName(UOM_MAP[record[7]]),
-                    "min_unit": record[9] == "N" and "box" or (record[9] == "S" and "both" or "unit"),
-                    "uos_coeff": record[10] and (1.0 / int(record[10])) or 1.0,
-                    "mes_type": record[9] == "V" and "variable" or "fixed",
-                    "un_ca": record[10] or 1.0,
-                    "supplier_un_ca": record[10] or 1.0,
-                    "supplier_kg_un": record[8] == "K" and 1.0 or 1.0,
-                    "kg_un": record[8] == "K" and 1.0 or 1.0,
-                    "taxes_id": [(6, 0, self.getTaxes(IVA_MAP[str(int(record[13]))][0]))],
-                    "supplier_taxes_id": [(6, 0, self.getTaxes(IVA_MAP[str(int(record[13]))][1]))],
-                    "standard_price": record[16],
-                    "cost_method": "average",
-                    "type": record[24] == "N" and "service" or "product",
-                    "weight": (record[9] == "N" and record[10]) and (record[30] / (1.0 / int(record[10]))) or (record[9] == "S" and record[30] or 0.0),
-                    "ca_ma": 1.0,
-                    "ma_pa": 1.0,
-                    "un_width": 1.0,
-                    "ma_width": 1.0,
-                    "ca_width": 1.0,
-                    "un_height": 1.0,
-                    "ma_height": 1.0,
-                    "ca_height": 1.0,
-                    "un_length": 1.0,
-                    "ca_length": 1.0,
-                    "ma_length": 1.0,
-                    "pa_height": 1.0,
-                    "supplier_un_width": 1.0,
-                    "supplier_un_height": 1.0,
-                    "supplier_un_length": 1.0,
-                    "supplier_ca_ma": 1.0,
-                    "supplier_ma_width": 1.0,
-                    "supplier_ma_height": 1.0,
-                    "supplier_ma_pa": 1.0,
-                    "supplier_ca_length": 1.0,
-                    "supplier_pa_height": 1.0,
-                    "supplier_ca_height": 1.0,
-                    "supplier_ca_width": 1.0,
-                    "purchase_ok": True,
-                    "sale_ok": True
-                }
-                product_ids = self.search("product.product", [('default_code', '=', product_vals["default_code"]),'|',('active', '=', True),('active', '=', False)])
-                if not product_ids:
-                    product_id = self.create("product.product", product_vals)
+                product_ids = self.search('product.product', [('default_code', '=', str(int(record[0]))),'|',('active', '=', True),('active', '=', False)])
+                if product_ids:
+                    self.write('product.product', product_ids[0], {'list_price': float(record[7])})
                 else:
-                    product_id = product_ids[0]
-                    self.write("product.product", [product_id], product_vals)
-
-
-                if record[25] or record[29]:
-                    ean13 = str(int((record[29] or record[25])))
-                    if len(ean13) == 13:
-                        try:
-                            self.write("product.product", [product_id], {'ean13': ean13})
-                        except:
-                            print u"EAN13 no váĺido", ean13
-
-                product_data = self.read("product.product", product_id, ["product_tmpl_id"])
-                supplier_id = self.getSupplierByRef(str(int(record[1])))
-                if supplier_id:
-                    supplier_vals = {
-                        "name": supplier_id,
-                        "product_code": record[3],
-                        "product_tmpl_id": product_data["product_tmpl_id"][0]
-                    }
-                    self.create("product.supplierinfo", supplier_vals)
-
-                self.exec_workflow("product.template", "logic_validated", product_data["product_tmpl_id"][0])
-                self.exec_workflow("product.template", "commercial_validated", product_data["product_tmpl_id"][0])
-                self.exec_workflow("product.template", "active", product_data["product_tmpl_id"][0])
+                    print "Producto no encontrado %s" % str(int(record[0]))
 
                 print "%s de %s" % (cont, all_lines)
                 cont += 1
@@ -273,6 +192,6 @@ class import_products(object):
 
 if __name__ == "__main__":
     if len(sys.argv) < 4:
-        print u"Uso: %s <dbname> <user> <password> <products.xls>" % sys.argv[0]
+        print u"Uso: %s <dbname> <user> <password> <prices.xls>" % sys.argv[0]
     else:
-        import_products(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+        import_prices(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
