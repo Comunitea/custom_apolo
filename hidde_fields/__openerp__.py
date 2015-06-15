035ec5f4f415cfef1e2bd58d7c74379ec3051ca5
jesusVMayor <jesus@comunitea.com>
2015-06-15 10:04:03 +0200

    [FIX]hidde_fields: Ocultados campos en vista en vez de modelo.

--- hidde_fields/__openerp__.py
+++ hidde_fields/__openerp__.py
@@ -26,7 +26,7 @@
     'description': """""",
     'author': 'Comunitea',
     'website': '',
-    "depends": ['base', 'product', 'midban_depot_stock', 'price_system_variable'],
+    "depends": ['base', 'product', 'midban_product', 'midban_depot_stock', 'price_system_variable'],
     "data": ['security/hidde_security.xml', 'product_view.xml'],
     "installable": True
 }
