f9ae44ebd4530fa8532c48e091cfaa151ad54819
javierjcf <javierjcf@gmail.com>
2015-10-28 20:17:51 +0100

    [ADD]set_order_location_tour: Campos para busqueda por camara pasillo altura y columna

--- set_order_location_tour/models/stock_location.py
+++ set_order_location_tour/models/stock_location.py
@@ -24,3 +24,29 @@ from openerp import models, fields, api
 class StockLocation(models.Model):
 
     _inherit = "stock.location"
+
+    xy_camera = fields.Char(compute='_get_coordinates_names', string="Camera",
+                            readonly=True, store=True)
+    xy_aisle = fields.Char(compute='_get_coordinates_names', string="Aisle",
+                          readonly=True, store=True)
+    xy_column = fields.Char(compute='_get_coordinates_names', string="Column",
+                            readonly=True, store=True)
+    xy_height = fields.Char(compute='_get_coordinates_names', string="Height",
+                            readonly=True, store=True)
+
+    @api.multi
+    @api.depends('bcd_name')
+    def _get_coordinates_names(self):
+        for loc in self:
+            loc.xy_camera = ""
+            loc.xy_aise = ""
+            loc.xy_column = ""
+            loc.xy_height = ""
+            if loc.bcd_name:
+                bcd_name_parts = loc.bcd_name.split(" ")
+                if len(bcd_name_parts) != 4:
+                    continue  # Not set if no correct format
+                loc.xy_camera = bcd_name_parts[0]
+                loc.xy_aisle = bcd_name_parts[1]
+                loc.xy_column = bcd_name_parts[2]
+                loc.xy_height = bcd_name_parts[3]
