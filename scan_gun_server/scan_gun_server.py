d4674eea3f95bfc9b8cb433f7a0bf91c5773cd4e
Kiko <kiko@comunitea.com>
2015-12-01 01:30:52 +0100

    [IMP] Cancelar Tareas Vacías de Ubicación

--- scan_gun_server/scan_gun_server.py
+++ scan_gun_server/scan_gun_server.py
@@ -2033,7 +2033,7 @@ class ScanGunProtocol(LineReceiver):
             total=0
 
         if (self.type =="ubication" or self.type == "reposition" ) and not self.ops:
-            strg =u'Tienes una tarea nueva\nLee paquetes a ubicar\n(%s) Finalizar Tarea'%KEY_FINISH
+            strg =u'Tienes una tarea nueva\nLee paquetes a ubicar\n%s Cancelar Tarea '%KEY_CANCEL
         else:
             header1=''
             if self.type!='picking':
@@ -3452,7 +3452,7 @@ class ScanGunProtocol(LineReceiver):
             order_line = False
 
 
-        if line == KEY_CANCEL and False:
+        if line == KEY_CANCEL:
             res = self.factory.odoo_con.gun_cancel_task(self.user_id, self.task_id)
             self.check_task()
             self.active_task = self.get_active_task()
@@ -3487,21 +3487,21 @@ class ScanGunProtocol(LineReceiver):
             self._snd(self.get_str_menu_task())
             return
 
-        if line ==KEY_PRINT:
-            print_ids=[]
-            inc = 0
-            for op_ in self.ops:
-                inc+=1
-                package_id = self.ops[op_]['pack_id']
-                print_ids.append[inc]({
-                    'id': package_id,
-                    'name':self.ops[op_]['paquete']
-                })
-            self.last_state = self.state
-            self.packs= print_ids
-            self.state = 'print_tags'
-            self._snd(self.get_str_print_tags())
-            return
+        # if line == KEY_PRINT and self.ops:
+        #     print_ids=[]
+        #     inc = 0
+        #     for op_ in self.ops:
+        #         inc+=1
+        #         package_id = self.ops[op_]['pack_id']
+        #         print_ids.append[inc]({
+        #             'id': package_id,
+        #             'name':self.ops[op_]['paquete']
+        #         })
+        #     self.last_state = self.state
+        #     self.packs= print_ids
+        #     self.state = 'print_tags'
+        #     self._snd(self.get_str_print_tags())
+        #     return
 
 
         if self.ops and not order_line:
@@ -3570,103 +3570,104 @@ class ScanGunProtocol(LineReceiver):
 
         if not self.ops:
             self.ops = self.factory.odoo_con.get_ops(self.user_id, self.task_id, self.type)
-        num_ops = len(self.ops)
-        op_=self.ops[str(self.active_op)]
-        self.op_id = op_['ID']
 
-        # if op_['VISITED'] == False:
-        #     op_['VISITED'] =True
-        #     res = self.factory.odoo_con.change_op_value(self.user_id, self.op_id, 'visited', True)
+        if self.ops:
+            num_ops = len(self.ops)
+            op_=self.ops[str(self.active_op)]
+            self.op_id = op_['ID']
 
-        not_vis = 0
-        not_proc = 0
-        for op in self.ops:
-            op__=self.ops[op]
-            if op__['VISITED']:
-                not_vis += 1
-            if op__['to_process']:
-                not_proc += 1
+            # if op_['VISITED'] == False:
+            #     op_['VISITED'] =True
+            #     res = self.factory.odoo_con.change_op_value(self.user_id, self.op_id, 'visited', True)
 
+            not_vis = 0
+            not_proc = 0
 
-        if op_['to_process']:
-            self.vals["paquete"]=op_['package']
-            self.vals["destino"]=op_['DESTINO']
-
-        if not op_:
-            raise Exception(u"No hay datos de la operacion\nImposible imprimir operacion")
-
-        header = "Tarea: %s OP: %s\nNo vistas %s Ok %s\n"\
-                 %(self.tasks[self.active_task]['ref'],
-                   str(self.op_id), str(num_ops-not_vis), str(not_proc))
-        header = u"%s: Faltan %s de %s\n" \
-                 u""%(self.tasks[self.active_task]['ref'], num_ops-not_proc , num_ops)
-        if self.tasks[self.active_task]['paused'] == True:
-            header = self.inverse(header)
-
-        # header += "Operation: " + str(op_['ID']) + "(" + str(self.active_op) \
-        #          + " de "  + str(len(self.ops)) + ")"+ "\n"
-        delimiter = "*"*25+"\n"
-        strg = header
-        strg += u"%s\n"%op_['product']
-
-        strg_ = u'%s:%s\n'%(op_['package'], op_['lot'])
-        if (self.step == 0 or self.step ==1) and not op_['to_process']:
-            strg +=self.inverse(strg_)
-        else:
-            strg += strg_
+            for op in self.ops:
+                op__=self.ops[op]
+                if op__['VISITED']:
+                    not_vis += 1
+                if op__['to_process']:
+                    not_proc += 1
 
 
+            if op_['to_process']:
+                self.vals["paquete"]=op_['package']
+                self.vals["destino"]=op_['DESTINO']
 
-        if not op_['CANTIDAD']==1:
-            strg += u"%s %s\n"%(op_['CANTIDAD'], op_['uom'])
-        if op_['lot']!='MultiPack':
-        #aqui pongo cantidades informativas
-            strg+=u"Mover: %s %s\n"%(op_['packed_qty'], op_['uom'])
-            if op_['uom_id']!=op_['uos_id']:
-                strg+="%s%s %s\n"%(' ' * 7, op_['uos_qty'], op_['uos'])
-        #Aqui pongo si es paquete completo
-        if op_['is_package']:
-            strg+=u"       (Paquete Completo)\n"
+            if not op_:
+                raise Exception(u"No hay datos de la operacion\nImposible imprimir operacion")
 
-        #Si la operacion no es de ubicacion pongo DE: ...
-        #En ubicacion no hace falta
-        if self.type != 'ubication':
-            strg_= u'De: %s %s\n'%(op_['origen_bcd'], str(op_['origen_id'])* self.show_id)
-            if self.step ==0 and not op_['to_process'] :
-                strg += self.inverse(strg_)
+            header = "Tarea: %s OP: %s\nNo vistas %s Ok %s\n"\
+                     %(self.tasks[self.active_task]['ref'],
+                       str(self.op_id), str(num_ops-not_vis), str(not_proc))
+            header = u"%s: Faltan %s de %s\n" \
+                     u""%(self.tasks[self.active_task]['ref'], num_ops-not_proc , num_ops)
+            if self.tasks[self.active_task]['paused'] == True:
+                header = self.inverse(header)
+
+            # header += "Operation: " + str(op_['ID']) + "(" + str(self.active_op) \
+            #          + " de "  + str(len(self.ops)) + ")"+ "\n"
+            delimiter = "*"*25+"\n"
+            strg = header
+            strg += u"%s\n"%op_['product']
+
+            strg_ = u'%s:%s\n'%(op_['package'], op_['lot'])
+            if (self.step == 0 or self.step ==1) and not op_['to_process']:
+                strg +=self.inverse(strg_)
             else:
-                strg +=  strg_
-        #Si la operacion no es de pincking pongo A: ...
-        #En picking no hace falta
-        if self.type == 'reposition':
-            strg_= u'A: %s %s\n'%(op_['destino_bcd'], str(op_['destino_id'])* self.show_id)
-            if (self.step==2 or self.step ==3) and not op_['to_process']:
-                strg += self.inverse(strg_)
-                op_ok = False
-            else:
-                strg +=strg_
-
-        if self.type == "ubication":
-            # import ipdb; ipdb.set_trace()
-            # if op_['DESTINO'] != op_['destino_bcd'] or op_['destino_bcd'] == "Almacen":
-            #     destino = u"%s (%s)"%(op_['destino_bcd'], op_['picking_location_id'])
-            # else:
-            #     destino = u"Almacen (%s)"%op_['destino_bcd']
-            #
-
-            destino =u"Almacen"
-            destino = u"%s (%s)"%("Almacen", op_['picking_location_id'] or '')
-            strg_= u'A: %s\n'%destino
+                strg += strg_
 
 
-            if (self.step==2 or self.step ==3) and not op_['to_process']:
-                strg += self.inverse(strg_)
-                op_ok = False
-            else:
-                strg +=strg_
 
-        if op_['to_process']:
-            strg += self.inverse(u"\n[x] %s Cancelar OP\n"%KEY_CANCEL)
+            if not op_['CANTIDAD']==1:
+                strg += u"%s %s\n"%(op_['CANTIDAD'], op_['uom'])
+            if op_['lot']!='MultiPack':
+            #aqui pongo cantidades informativas
+                strg+=u"Mover: %s %s\n"%(op_['packed_qty'], op_['uom'])
+                if op_['uom_id']!=op_['uos_id']:
+                    strg+="%s%s %s\n"%(' ' * 7, op_['uos_qty'], op_['uos'])
+            #Aqui pongo si es paquete completo
+            if op_['is_package']:
+                strg+=u"       (Paquete Completo)\n"
+
+            #Si la operacion no es de ubicacion pongo DE: ...
+            #En ubicacion no hace falta
+            if self.type != 'ubication':
+                strg_= u'De: %s %s\n'%(op_['origen_bcd'], str(op_['origen_id'])* self.show_id)
+                if self.step ==0 and not op_['to_process'] :
+                    strg += self.inverse(strg_)
+                else:
+                    strg +=  strg_
+            #Si la operacion no es de pincking pongo A: ...
+            #En picking no hace falta
+            if self.type == 'reposition':
+                strg_= u'A: %s %s\n'%(op_['destino_bcd'], str(op_['destino_id'])* self.show_id)
+                if (self.step==2 or self.step ==3) and not op_['to_process']:
+                    strg += self.inverse(strg_)
+                    op_ok = False
+                else:
+                    strg +=strg_
+
+            if self.type == "ubication":
+                # if op_['DESTINO'] != op_['destino_bcd'] or op_['destino_bcd'] == "Almacen":
+                #     destino = u"%s (%s)"%(op_['destino_bcd'], op_['picking_location_id'])
+                # else:
+                #     destino = u"Almacen (%s)"%op_['destino_bcd']
+                #
+
+                destino =u"Almacen"
+                destino = u"%s (%s)"%("Almacen", op_['picking_location_id'] or '')
+                strg_= u'A: %s\n'%destino
+
+
+                if (self.step==2 or self.step ==3) and not op_['to_process']:
+                    strg += self.inverse(strg_)
+                    op_ok = False
+                else:
+                    strg +=strg_
+            if op_['to_process']:
+                strg += self.inverse(u"\n[x] %s Cancelar OP\n"%KEY_CANCEL)
 
         keys = ""
         keys += u"%s Atras"%KEY_VOLVER
