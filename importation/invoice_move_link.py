e8205e0d48a140cb747ace6411a72be478076357
Santi Argüeso <santi@comunitea.com>
2015-12-05 21:50:25 +0100

    [Importacion]

--- importation/invoice_move_link.py
+++ importation/invoice_move_link.py
@@ -202,7 +202,7 @@ class invoice_move(object):
                     if len(move_ids) == 1:
                         print u"ENCONTRADA REFERENCIA para %s"%(invoice['number'])
                         moves = self.read('account.move.line', move_ids, ['move_id', 'debit'])
-                        if self.isclose(moves[0]['debit'], invoice['amount_total'],0.2,0.2):
+                        if self.isclose(moves[0]['debit'], invoice['amount_total'],abs_tol=0.02):
                             if invoice['date_due']:
                                 date_due = invoice['date_due']
                             else:
