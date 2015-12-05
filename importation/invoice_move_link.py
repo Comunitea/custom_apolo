e594d4423b8ab56e7186f955628677a722308f66
Santi Argüeso <santi@comunitea.com>
2015-12-05 21:35:15 +0100

    [IMP] Mejora y log para script enlace factura - asiento

--- importation/invoice_move_link.py
+++ importation/invoice_move_link.py
@@ -171,6 +171,9 @@ class invoice_move(object):
 
     def invoice_move_link(self):
         try:
+            logfile_diff = open('log_invoices_amount_diff.csv', 'w') # Indicamos el valor 'w'.
+            logfile_no_found = open('log_invoices_no_found.csv', 'w')
+
             invoice_ids = self.search('account.invoice', [('state','=','history'),
                                                           ('type','=','out_invoice'),
                                                           ('amount_total', '>', 0),
@@ -191,43 +194,49 @@ class invoice_move(object):
                 move_ref_post = move_ref_post[-7:]
                 move_ref = move_ref_pre + move_ref_post
                 domain = [('account_id.type', '=', 'receivable'),
-                        ('debit', '>=', invoice['amount_total']-0.2),
-                          ('debit', '<=', invoice['amount_total']+0.2),
                         ('ref', 'like', move_ref)]
                 print "Buscando %s : %s - %s  "%(move_ref,invoice['date_invoice'], invoice['amount_total'])
                 move_ids = self.search('account.move.line',domain)
 
                 if move_ids:
                     if len(move_ids) == 1:
-                        moves = self.read('account.move.line', move_ids, ['move_id'])
-                        if invoice['date_due']:
-                            date_due = invoice['date_due']
+                        print u"ENCONTRADA REFERENCIA para %s"%(invoice['number'])
+                        moves = self.read('account.move.line', move_ids, ['move_id', 'debit'])
+                        if self.isclose(moves[0]['debit'], invoice['amount_total'],0.2,0.2):
+                            if invoice['date_due']:
+                                date_due = invoice['date_due']
+                            else:
+                                date_due = invoice['date_invoice']
+                            vals = {
+                                'move_id': moves[0]['move_id'][0],
+                                'date_due': date_due
+                            }
+                            self.write('account.invoice', invoice['id'], vals)
+                            vals = {
+                                'date_maturity': date_due
+                            }
+                            self.write('account.move.line', move_ids[0], vals)
+                            print u"HECHO   para %s %s"%(invoice['number'],invoice['amount_total'])
+                            realizadas += 1
                         else:
-                            date_due = invoice['date_invoice']
-                        vals = {
-                            'move_id': moves[0]['move_id'][0],
-                            'date_due': date_due
-                        }
-                        self.write('account.invoice', invoice['id'], vals)
-                        vals = {
-                            'date_maturity': date_due
-                        }
-                        self.write('account.move.line', move_ids[0], vals)
-                        print u"HECHO   para %s"%(invoice['number'])
-                        realizadas += 1
+                            print u"IMPORTES DIFERENTES para %s: %s(asiento) -  %s(factura)"%(invoice['number'],
+                                                                                              moves[0]['debit'], invoice['amount_total'])
+                            logfile_diff.write("%s,%s,%s\n"%(invoice['number'],moves[0]['debit'], invoice['amount_total']))
                     else:
                         print u"Más de un Asiento no encontrado  para %s"%(invoice['number'])
                 else:
                     print u"Asiento no encontrado  para %s"%(invoice['number'])
+                    logfile_no_found.write("%s\n"%(invoice['number']))
                 num +=1
                 print u"Procesado %s : %s/%s/%s"%(invoice['number'],realizadas, num, total)
             print "FINALIZado!!. No conciliados procesados TOTAL:"
-
+            logfile_diff.close()
+            logfile_no_found.close()
         except Exception, e:
             print u"EXCEPTION: REC %s"%(e)
             print "No conciliados procesados hasta el momento:"
-
-
+            logfile_diff.close()
+            logfile_no_found.close()
         return True
 
 
