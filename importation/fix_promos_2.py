cd31e4e6cdd9f667192bfe32f163e79b78e9d6fb
Santi Argüeso <santi@comunitea.com>
2015-12-10 18:58:28 +0100

    [IMPO] script para corregir promociones

---
+++ importation/fix_promos_2.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import xmlrpclib
+import socket
+import traceback
+
+class fix_promos(object):
+    def __init__(self, dbname, user, passwd):
+        """método incial"""
+
+        try:
+            self.url_template = "http://%s:%s/xmlrpc/%s"
+            self.server = "localhost"
+            self.port = 9069
+            self.dbname = dbname
+            self.user_name = user
+            self.user_passwd = passwd
+
+            #
+            # Conectamos con OpenERP
+            #
+            login_facade = xmlrpclib.ServerProxy(self.url_template % (self.server, self.port, 'common'))
+            self.user_id = login_facade.login(self.dbname, self.user_name, self.user_passwd)
+            self.object_facade = xmlrpclib.ServerProxy(self.url_template % (self.server, self.port, 'object'))
+
+            res = self.fix_promos()
+            #con exito
+            if res:
+                print ("All created")
+        except Exception, e:
+            print ("ERROR: ", (e))
+            sys.exit(1)
+
+        #Métodos Xml-rpc
+
+    def exception_handler(self, exception):
+        """Manejador de Excepciones"""
+        print "HANDLER: ", (exception)
+        return True
+
+    def create(self, model, data, context={}):
+        """
+        Wrapper del metodo create.
+        """
+        try:
+            res = self.object_facade.execute(self.dbname, self.user_id, self.user_passwd,
+                                                            model, 'create', data, context)
+            return res
+        except socket.error, err:
+            raise Exception(u'Conexion rechazada: %s!' % err)
+        except xmlrpclib.Fault, err:
+            raise Exception(u'Error %s en create: %s' % (err.faultCode, err.faultString))
+
+
+    def search(self, model, query, offset=0, limit=False, order=False, context={}, count=False, obj=1):
+        """
+        Wrapper del metodo search.
+        """
+        try:
+            ids = self.object_facade.execute(self.dbname, self.user_id, self.user_passwd,
+                                    model, 'search', query, offset, limit, order, context, count)
+            return ids
+        except socket.error, err:
+                raise Exception(u'Conexion rechazada: %s!' % err)
+        except xmlrpclib.Fault, err:
+                raise Exception(u'Error %s en search: %s' % (err.faultCode, err.faultString))
+
+
+    def read(self, model, ids, fields, context={}):
+        """
+        Wrapper del metodo read.
+        """
+        try:
+            data = self.object_facade.execute(self.dbname, self.user_id, self.user_passwd,
+                                            model, 'read', ids, fields, context)
+            return data
+        except socket.error, err:
+                raise Exception(u'Conexion rechazada: %s!' % err)
+        except xmlrpclib.Fault, err:
+                raise Exception(u'Error %s en read: %s' % (err.faultCode, err.faultString))
+
+
+    def write(self, model, ids, field_values,context={}):
+        """
+        Wrapper del metodo write.
+        """
+        try:
+            res = self.object_facade.execute(self.dbname, self.user_id, self.user_passwd,
+                                                    model, 'write', ids, field_values, context)
+            return res
+        except socket.error, err:
+                raise Exception(u'Conexion rechazada: %s!' % err)
+        except xmlrpclib.Fault, err:
+                raise Exception(u'Error %s en write: %s' % (err.faultCode, err.faultString))
+
+
+    def unlink(self, model, ids, context={}):
+        """
+        Wrapper del metodo unlink.
+        """
+        try:
+            res = self.object_facade.execute(self.dbname, self.user_id, self.user_passwd,
+                                                    model, 'unlink', ids, context)
+            return res
+        except socket.error, err:
+                raise Exception(u'Conexion rechazada: %s!' % err)
+        except xmlrpclib.Fault, err:
+                    raise Exception(u'Error %s en unlink: %s' % (err.faultCode, err.faultString))
+
+    def default_get(self, model, fields_list=[], context={}):
+        """
+        Wrapper del metodo default_get.
+        """
+        try:
+            res = self.object_facade.execute(self.dbname, self.user_id, self.user_passwd,
+                                        model, 'default_get', fields_list, context)
+            return res
+        except socket.error, err:
+                raise Exception('Conexion rechazada: %s!' % err)
+        except xmlrpclib.Fault, err:
+                raise Exception('Error %s en default_get: %s' % (err.faultCode, err.faultString))
+
+    def execute(self, model, method, *args, **kw):
+        """
+        Wrapper del método execute.
+        """
+        try:
+            res = self.object_facade.execute(self.dbname, self.user_id, self.user_passwd,
+                                                                model, method, *args, **kw)
+            return res
+        except socket.error, err:
+                raise Exception('Conexión rechazada: %s!' % err)
+        except xmlrpclib.Fault, err:
+                raise Exception('Error %s en execute: %s' % (err.faultCode, err.faultString))
+
+    def exec_workflow(self, model, signal, ids):
+        """ejecuta un workflow por xml rpc"""
+        try:
+            res = self.object_facade.exec_workflow(self.dbname, self.user_id, self.user_passwd, model, signal, ids)
+            return res
+        except socket.error, err:
+            raise Exception(u'Conexión rechazada: %s!' % err)
+        except xmlrpclib.Fault, err:
+            raise Exception(u'Error %s en exec_workflow: %s' % (err.faultCode, err.faultString))
+
+    def getSupplierByRef(self, partner_ref):
+        partner_ids = self.search("res.partner", [('supplier','=',True),('ref','=',partner_ref)])
+        return partner_ids and partner_ids[0] or False
+
+    def getTaxes(self, tax_name):
+        tax_ids = self.search("account.tax", [('name', '=', tax_name)])
+        return tax_ids
+
+    def getUomByName(self, uom_name):
+        uom_ids = self.search("product.uom", [('name', '=', uom_name)])
+        return uom_ids and uom_ids[0] or False
+
+    def getCategoryByName(self, categ_name):
+        categ_ids = self.search("product.category", [('name', '=', categ_name)])
+        if not categ_ids:
+            categ_id = self.create("product.category", {"name": categ_name})
+            categ_ids = [categ_id]
+        return categ_ids and categ_ids[-1] or False
+
+    def fix_promos(self):
+        try:
+            condition_ids = self.search('promos.rules', [])
+            conditions = self.read('promos.rules', condition_ids,
+                                  ['id', 'expressions',
+                                  'actions' ])
+            print u"Iniciando: %s"%len(condition_ids)
+            cond_num = 1
+            for condition in conditions:
+                print u"Procesando : %s / %s"%(cond_num, len(condition_ids))
+                actions = self.read('promos.rules.actions', condition['actions'],
+                                  ['id', 'product_code','action_type'])
+                if actions[0]['action_type'] == 'prod_disc_perc_sub':
+                    recorre = 0
+                    for action in actions:
+                        cond_exp_id = condition['expressions'][recorre]
+                        self.write('promos.rules.conditions.exps', cond_exp_id,
+                                   {'value': action['product_code']})
+                        recorre += 1
+                cond_num += 1
+
+        except Exception, e:
+            print "EXCEPTION: REC: %s",(e)
+        return True
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print u"Uso: %s <dbname> <user> <password> " % sys.argv[0]
+    else:
+        fix_promos(sys.argv[1], sys.argv[2], sys.argv[3])
